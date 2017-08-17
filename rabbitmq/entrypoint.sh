37488402266fe0207da9962464c4ce1fc1ba8818
lvluqi <dstack@163.com>
2017-08-17 14:10:12 +0800

    final

--- rabbitmq/entrypoint.sh
+++ rabbitmq/entrypoint.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
-chown -R rabbitmq:rabbitmq /var/lib/rabbitmq && \
-      rabbitmqctl add_user lvluqi lvluqi && /usr/sbin/rabbitmqctl set_user_tags lvluqi monitoring && \
-      /usr/bin/supervisord
+chown -R rabbitmq:rabbitmq /var/lib/rabbitmq && \ 
+      /usr/bin/supervisord && \
+      rabbitmqctl add_user lvluqi lvluqi && /usr/sbin/rabbitmqctl set_user_tags lvluqi administrator
