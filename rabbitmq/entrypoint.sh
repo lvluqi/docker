fce1b0df578b056b5eb6a4b245784637aa50541a
lvluqi <dstack@163.com>
2017-08-17 13:04:44 +0800

    fix Dockerfile of mongodb

--- rabbitmq/entrypoint.sh
+++ rabbitmq/entrypoint.sh
@@ -1,4 +1,5 @@
 #/bin/bash
 
 chown -R rabbitmq:rabbitmq /var/lib/rabbitmq && \
+      rabbitmqctl add_user lvluqi lvluqi && /usr/sbin/rabbitmqctl set_user_tags lvluqi monitoring && \
       /usr/bin/supervisord
