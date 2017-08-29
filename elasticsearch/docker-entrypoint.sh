426704939c8f7a5c748ec2eeb00f11875356807e
lvluqi <dstack@163.com>
2017-08-29 11:45:40 +0800

    success

--- elasticsearch/docker-entrypoint.sh
+++ elasticsearch/docker-entrypoint.sh
@@ -17,7 +17,7 @@ CLASS_PATH=$JAVA_HOME/lib:$JAVA_HOME/jre/lib
 PATH=$PATH:$JAVA_HOME/bin
 export JAVA_HOME
 EOF
-  if [ -d "$ES_DIR/search-guard-ssl" ];then
+  if [ ! -d "/var/lib/elasticsearch/private" ];then
      ln -s /etc/elasticsearch/ $ES_DIR/config && ln -s /var/lib/elasticsearch $ES_DIR/data && ln -s /var/log/elasticsearch $ES_DIR/logs
     for path in \
 	/usr/share/elasticsearch/data \
@@ -46,4 +46,5 @@ EOF
      [ $? -eq 0 ] && cd && rm -rf $ES_DIR/search-guard-ssl
   fi
 fi
+cd
 exec "$@"
